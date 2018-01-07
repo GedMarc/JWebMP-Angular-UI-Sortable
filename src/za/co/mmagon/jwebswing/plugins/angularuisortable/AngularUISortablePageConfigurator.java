0b491f1171de4414f3bb7397620ddd8c5ffe934c
Marc Magon <ged_marc@hotmail.com>
2018-01-07 15:04:37 +0200

    0.18.0.1Event ID Data Carriers, Angular Forms Inclusion and Update for a more generecised implementation

--- src/za/co/mmagon/jwebswing/plugins/angularuisortable/AngularUISortablePageConfigurator.java
+++ src/za/co/mmagon/jwebswing/plugins/angularuisortable/AngularUISortablePageConfigurator.java
@@ -27,9 +27,9 @@ import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
 		pluginDownloadUrl = "https://sourceforge.net/projects/jwebswing/files/plugins/AngularUISortable.jar/download"
 ) class AngularUISortablePageConfigurator extends PageConfigurator
 {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	/*
 	 * Constructs a new AngularUISortablePageConfigurator
 	 */
@@ -37,15 +37,15 @@ import za.co.mmagon.jwebswing.plugins.jquery.JQueryPageConfigurator;
 	{
 		//Nothing needed
 	}
-	
+
 	@Override
 	public Page configure(Page page)
 	{
 		if (!page.isConfigured())
 		{
-			JQueryPageConfigurator.setRequired(page.getBody(), true);
-			AngularPageConfigurator.setRequired(page.getBody(), true);
-			
+			JQueryPageConfigurator.setRequired(true);
+			AngularPageConfigurator.setRequired(true);
+
 			page.getBody().addJavaScriptReference(AngularUISortableReferencePool.AngularUISortable.getJavaScriptReference());
 			page.getAngular().getAngularModules().add(new AngularUISortableModule());
 		}
