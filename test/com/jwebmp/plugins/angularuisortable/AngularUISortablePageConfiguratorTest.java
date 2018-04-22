09b61c9827309a1dc466ad140519114ec602e077
GedMarc <ged_marc@hotmail.com>
2018-04-22 15:47:26 +0200

    Prepping for RC1 - Update Base Package Name to new Name

--- test/com/jwebmp/plugins/angularuisortable/AngularUISortablePageConfiguratorTest.java
+++ test/com/jwebmp/plugins/angularuisortable/AngularUISortablePageConfiguratorTest.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.angularuisortable;
 
-import com.jwebmp.BaseTestClass;
 import com.jwebmp.Page;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,7 +8,7 @@ import org.junit.jupiter.api.Test;
  * @author Marc Magon
  */
 public class AngularUISortablePageConfiguratorTest
-		extends BaseTestClass
+
 {
 
 	public AngularUISortablePageConfiguratorTest()
@@ -19,7 +18,7 @@ public class AngularUISortablePageConfiguratorTest
 	@Test
 	public void testSomeMethod()
 	{
-		Page p = getInstance();
+		Page p = new Page();
 		System.out.println(p.toString(0));
 		Assertions.assertTrue(p.toString(0)
 		                       .contains("<script src=\"bower_components/angular-ui-sortable/sortable.js\" type=\"text/javascript\"></script>"));
