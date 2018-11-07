fe653e6e002660fd67e5ee3287129c3c6108b8bc
GedMarc <ged_marc@hotmail.com>
2018-11-07 03:35:13 +0200

    Deeper enablement support

--- src/main/java/com/jwebmp/plugins/angularuisortable/AngularUISortableModule.java
+++ src/main/java/com/jwebmp/plugins/angularuisortable/AngularUISortableModule.java
@@ -45,4 +45,15 @@ public class AngularUISortableModule
 	{
 		return null;
 	}
+
+	/**
+	 * If this page configurator is enabled
+	 *
+	 * @return if the configuration must run
+	 */
+	@Override
+	public boolean enabled()
+	{
+		return AngularUISortablePageConfigurator.isEnabled();
+	}
 }
